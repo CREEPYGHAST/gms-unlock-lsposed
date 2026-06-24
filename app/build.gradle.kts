85f4f35700673ae0e10e81864d6eab6f9e298994
CREEPYGHAST <46750333+CREEPYGHAST@users.noreply.github.com>
2026-06-24 23:27:51 +0800

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.codex.gmsunlock"
         minSdk = 27
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildFeatures {
